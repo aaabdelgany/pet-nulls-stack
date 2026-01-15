d75c4260137e34d1175943143bc62042bdd15af4
Abdurahman Abdelgany <abdu.abdelgany@hashicorp.com>
2026-01-15 14:15:17 -0500

    randos

--- pet/main.tf
+++ pet/main.tf
@@ -1,15 +1,6 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-terraform {
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.3.2"
-    }
-  }
-}
-
 variable "prefix" {
   type = string
 }
